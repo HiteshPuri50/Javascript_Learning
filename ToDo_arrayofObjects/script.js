331e661207ee62731e1642360d818d1ffdfb329e
HiteshPuri50 <57486924+HiteshPuri50@users.noreply.github.com>
2022-09-04 19:46:42 +0530

    Child node can be inserted

--- ToDo_arrayofObjects/script.js
+++ ToDo_arrayofObjects/script.js
@@ -1,25 +1,19 @@
+let items = [];
 const tasks = {
-    items: [],
-    task: {
-        desc: "",
-
-    },
-    children: {
-        child: ''
-    },
     element: document.getElementById("newresult"),
     add: function (desc) {
-        this.task.desc = desc;
-        this.items.push(this.task);
-        console.log(this.items);
-        this.render();
+        items.push({
+            desc,
+        },);
+        console.log(items);
+        this.render(desc);
     },
-    render: function () {
-        console.log(this.items);
+    render: function (desc) {
+        console.log(items);
         const li = document.createElement('li');
         const deletee = document.createElement('button');
         const addChild = document.createElement('button');
-        li.textContent = this.task.desc;
+        li.textContent = `${desc}`;
         li.append(deletee);
         li.append(addChild);
         deletee.innerHTML = "Delete";
@@ -34,27 +28,19 @@ const tasks = {
             console.log(tasks);
             children = prompt("Please Enter the Task Child", "");
             if (children != '') {
-                console.log(tasks.children);
-                tasks.children = children;
-                tasks.children.child = children;
-                tasks.items.push(tasks.children);
+                console.log(children);
+                items.push({
+                    children
+                },);
                 tasks.renderChild();
             }
-
         })
     },
-
     renderChild: function () {
         let ul = document.createElement('ul');
         this.element.appendChild(ul);
         let li1 = document.createElement('li');
         ul.append(li1)
         li1.append(children);
-    },
-    sort: function() {
-        console.log(tasks.items);
-        sort = tasks.items.sort();
-        console.log(sort);
-        document.getElementById('newresult').innerHTML = sort;
+    },     
     }
-}
